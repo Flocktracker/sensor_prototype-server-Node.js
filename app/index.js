97d8a96608ae4b542988e03806560b40165a7030
Daniel Heriberto Palencia Arreola <buzoherbert@gmail.com>
2017-05-11 23:22:59 -0400

    Saving to postgres

--- app/index.js
+++ app/index.js
@@ -4,19 +4,35 @@ const bodyParser = require('body-parser')
 const app = express()  
 const port = 3000
 
+const pg = require('pg')  
+const conString = 'postgres://postgres:12345678@localhost/ph_data'
+
 const readings = []
 
 app.use(bodyParser.json());
 
-app.post('/ph_data', function (req, res) {  
+app.post('/ph_data', function (req, res, next) {  
     // retrieve user posted data from the body
     const reading = req.body
-    readings.push({
-      time: reading.time,
-      ph: reading.ph
-    })
-    res.send('successfully registered')
+    
+  	pg.connect(conString, function (err, client, done) {
+	    if (err) {
+	      // pass the error to the express error handler
+	      return next(err)
+	    }
+	    client.query('INSERT INTO readings (time_stamp, ph) VALUES ($1, $2);', [reading.time_stamp, reading.ph], function (err, result) {
+	    	done() //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
+
+	      	if (err) {
+	        	// pass the error to the express error handler
+	        	return next(err)
+	      	}
+
+	      	res.sendStatus(200)
+		})
+	})
 })
+
 
 app.get('/ph_data', function (req, res) {  
     // retrieve user posted data from the body
@@ -26,5 +42,4 @@ app.get('/ph_data', function (req, res) {
 })
 
 
-
-app.listen(port)  
+app.listen(port)
